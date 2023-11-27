6fddee3db5f60752d0708e8e083c87ec5b6eb462
Vincenzo Nikodem Cesqui Podiani <58045801+Rexeref@users.noreply.github.com>
2023-11-27 17:47:18 +0100

    Test per fare capire i pull a fox

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const ip = "127.0.0.1";
 let numClienti = 0;
 const users = [];
 
+// MODIFICA DI TEST
+
 function requestHandler(request, response) {
     fs.readFile('index.html', function (error, data) {
         if (error) {
